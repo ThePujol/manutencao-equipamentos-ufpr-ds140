311993e0c60f94106a5053cefd263961c509c789
vkataguiri <viniciuskataguiri@gmail.com>
2025-04-10 19:24:06 -0300

    refactor: faz o redesign da sidebar do cliente

--- frontend/src/app/components/ui/sidebar-cliente/sidebar-cliente.component.ts
+++ frontend/src/app/components/ui/sidebar-cliente/sidebar-cliente.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-cliente',
-  imports: [MatIconModule, RouterLink, RouterOutlet],
+  imports: [MatIconModule, RouterLink, RouterOutlet, RouterModule],
   templateUrl: './sidebar-cliente.component.html',
 })
 export class SidebarClienteComponent {
   @Input() nome: string = "Nome"
+  @Input() selected: number = 0;
 }
