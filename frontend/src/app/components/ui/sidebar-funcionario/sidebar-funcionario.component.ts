498f1388760565a715e4234c3fc40c38b781d786
Diego Pujol <diego_p_alvares@hotmail.com>
2025-05-20 23:57:40 -0300

    fix(relatorios): corrigir import e ícone do submenu dia

--- frontend/src/app/components/ui/sidebar-funcionario/sidebar-funcionario.component.ts
+++ frontend/src/app/components/ui/sidebar-funcionario/sidebar-funcionario.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { matExpandLessOutline, matExpandMoreOutline, matLogOutOutline } from '@ng-icons/material-icons/outline';
+import { matExpandLessOutline, matExpandMoreOutline, matLogOutOutline, matTodayOutline } from '@ng-icons/material-icons/outline';
 import { CommonModule } from '@angular/common';
 import { LoggedUserService } from '../../../services/logged-user.service';
 import { Funcionario } from '../../../shared/models/funcionario.model';
@@ -11,11 +11,12 @@ import { SidebarButtonComponent } from '../buttons/sidebar-button/sidebar-button
 @Component({
 	selector: 'app-sidebar-funcionario',
 	imports: [CommonModule, RouterLink, RouterOutlet, RouterModule, SidebarButtonComponent, NgIcon],
-	viewProviders: [
+		viewProviders: [
 		provideIcons({
 			matLogOutOutline,
 			matExpandMoreOutline,
-			matExpandLessOutline
+			matExpandLessOutline,
+			matTodayOutline,
 		}),
 	],
 	templateUrl: './sidebar-funcionario.component.html',
